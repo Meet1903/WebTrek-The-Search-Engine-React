43b273837c44ed07e59b845c5af631d6f864efd7
Meet1903 <meet.diwan.md@gmail.com>
2024-06-18 15:23:21 -0400

    Background video component

---
+++ client/webtrek/src/components/Backgroundvideo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import BackgroundVideoSrc from '../assets/background-video.mp4'
+
+export default function BackgroundVideo() {
+    return (
+        <video autoplay loop muted>
+          <source src={BackgroundVideoSrc} type="video/mp4" />
+        </video>
+    );
+}
