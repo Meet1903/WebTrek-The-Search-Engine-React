894f7ec54e3398a916dcd9057ef6b3ecc71cf0b1
Meet1903 <meet.diwan.md@gmail.com>
2024-06-18 15:26:42 -0400

    Issue with autoplay resolved

--- client/webtrek/src/components/Backgroundvideo.js
+++ client/webtrek/src/components/Backgroundvideo.js
@@ -3,7 +3,7 @@ import BackgroundVideoSrc from '../assets/background-video.mp4'
 
 export default function BackgroundVideo() {
     return (
-        <video autoplay loop muted>
+        <video autoPlay loop muted>
           <source src={BackgroundVideoSrc} type="video/mp4" />
         </video>
     );
