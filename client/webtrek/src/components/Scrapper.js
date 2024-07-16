f02c580c3c85994dd54b3c2d1a7b837e12f32083
Meet1903 <meet.diwan.md@gmail.com>
2024-07-16 00:23:05 -0400

    Add loading spinner to Scrapper component

--- client/webtrek/src/components/Scrapper.js
+++ client/webtrek/src/components/Scrapper.js
@@ -10,32 +10,11 @@ export default function Scraper() {
     const [url, setUrl] = useState('');
     const [path, setPath] = useState('');
 
-    useEffect(() => {
-        const form = document.getElementById("scraping-form");
-        if (form) {
-            form.addEventListener("submit", showLoadingSpinner);
-        }
-
-        return () => {
-            if (form) {
-                form.removeEventListener("submit", showLoadingSpinner);
-            }
-        };
-    }, []);
-
-    const showLoadingSpinner = () => {
-        let loadingSpinner = document.querySelector(".loader");
-        if (loadingSpinner) {
-            loadingSpinner.style.display = "block";
-        }
-        let container = document.querySelector(".container");
-        if (container) {
-            container.style.display = "block";
-        }
-    }
 
     const scrape = async (url, path) => {
         try {
+            document.getElementById('loader').classList.remove('hidden');
+            document.getElementById('visible-container').classList.add('hidden');
             const response = await axios.post(`/scrapper`, null, {
                 params: { url, path }
             });
@@ -44,6 +23,9 @@ export default function Scraper() {
             document.getElementById("message").innerHTML = data.message;
         } catch (error) {
             console.error("Error scraping:", error);
+        } finally {
+            document.getElementById('loader').classList.add('hidden');
+            document.getElementById('visible-container').classList.remove('hidden');
         }
     }
 
@@ -64,42 +46,45 @@ export default function Scraper() {
                     </a>
                 </h1>
             </div>
-            <div className="button-2-container">
-                <Link to="/">
-                    <button className="button-1">Back to Search Engine</button>
-                </Link>
+            <div className="loader-container">
+                <div className="loader hidden" id="loader"></div>
             </div>
-            <div className="loader" id="loader"></div>
-            <div className="container" id="container"></div>
-            <form method="post" id="scraping-form" className="regular-form" onSubmit={handleScrape}>
-                <label for="url">Enter comma separated domains:</label><br />
-                <input
-                    type="text"
-                    id="url"
-                    name="url"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
-                    class="form-text"
-                    style={{ marginBottom: '20px' }}
-                    required
-                />
-                <br />
-                <label for="path">Enter destination folder path:</label><br />
-                <input
-                    type="text"
-                    id="path"
-                    name="path"
-                    value={path}
-                    onChange={(e) => setPath(e.target.value)}
-                    class="form-text"
-                    style={{ marginBottom: '20px' }}
-                    required
-                />
-                <br />
-                <input type="hidden" id="folder_path" name="folder_path" class="form-text" />
-                <button type="submit" id="scrapper" class="submit-button">Scrape</button>
-            </form>
-            <h4 id="message" className="message"></h4>
+            <div className="" id="visible-container">
+                <div className="button-2-container">
+                    <Link to="/">
+                        <button className="button-1">Back to Search Engine</button>
+                    </Link>
+                </div>
+                <form method="post" id="scraping-form" className="regular-form" onSubmit={handleScrape}>
+                    <label for="url">Enter comma separated domains:</label><br />
+                    <input
+                        type="text"
+                        id="url"
+                        name="url"
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}
+                        class="form-text"
+                        style={{ marginBottom: '20px' }}
+                        required
+                    />
+                    <br />
+                    <label for="path">Enter destination folder path:</label><br />
+                    <input
+                        type="text"
+                        id="path"
+                        name="path"
+                        value={path}
+                        onChange={(e) => setPath(e.target.value)}
+                        class="form-text"
+                        style={{ marginBottom: '20px' }}
+                        required
+                    />
+                    <br />
+                    <input type="hidden" id="folder_path" name="folder_path" class="form-text" />
+                    <button type="submit" id="scrapper" class="submit-button">Scrape</button>
+                </form>
+            </div>
+            <p id="message" className="message"></p>
         </div>
     )
 }
