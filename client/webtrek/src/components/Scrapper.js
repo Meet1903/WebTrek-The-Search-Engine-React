1a2e650e4fb37b9d72737d321cbdf1d13131338b
Meet1903 <meet.diwan.md@gmail.com>
2024-07-15 23:05:46 -0400

    Basic scrapper component

--- client/webtrek/src/components/Scrapper.js
+++ client/webtrek/src/components/Scrapper.js
@@ -1,7 +1,20 @@
 import React from "react"
+import BackgroundImage from '../assets/background-image.png'
+import WebTrekLogoFont from '../assets/webtrek-font.png'
 
 export default function Scraper() { 
     return (
-        <h1>Scraper</h1>
+        <div>
+            <div className="background-image">
+                <img src={BackgroundImage} alt="" srcset="" />
+            </div>
+            <div className="header">
+                <h1>
+                    <a href="/" className="title-name">
+                        <img src={WebTrekLogoFont} alt="WebTrek" className="webtrek-font" />
+                    </a>
+                </h1>
+            </div>
+        </div>   
     )
 }
