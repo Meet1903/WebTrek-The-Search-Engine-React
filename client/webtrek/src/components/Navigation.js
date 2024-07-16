652e801ec3c8f4e717609dd269cc8aa9fecbeb84
Meet1903 <meet.diwan.md@gmail.com>
2024-07-15 23:06:23 -0400

    Updated Navigation component

--- client/webtrek/src/components/Navigation.js
+++ client/webtrek/src/components/Navigation.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Scrapper from './Scrapper';
-import History from './History';
 
 export default function Navigation() { 
     return (
     <>
      <div className="navigation-buttons">
-        <button class="button-1">Go to Scraper</button>
+        <Link to='/scrapper'><button class="button-1">Go to Scraper</button></Link>
         <Link to='/history'><button class="button-1">History</button></Link>
     </div>
     </>
